7e7d4d98a32c5dbbc0ac2df0ade2bc787549e23e
kieran <abc@gmail.com>
2026-01-09 10:32:01 +0700

    adding mode and scipy module

--- numpy_1.py
+++ numpy_1.py
@@ -1,9 +1,11 @@
 import numpy as np
+import scipy.stats as stats
 
 speed = [10,20,30,40,50,60,70,80,90,100]
 
 spe_mean = np.mean(speed)
 spe_median = np.median(speed)
-
+spe_mode = stats.mode(speed)
 print (spe_mean)
 print (spe_median)
+print (spe_mode)
