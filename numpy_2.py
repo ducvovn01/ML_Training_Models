95e2f36928f754ed721c850f2099b88d13e90983
kieran <abc@gmail.com>
2026-01-09 10:39:10 +0700

    Adding comments

--- numpy_2.py
+++ numpy_2.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 a1 = [10, 15, 20, 25, 30]
-
+# độ lệch chuẩn: standard deviation
 a1_std = np.std(a1)
 
-#studying variance
+#studying variance: phương sai
 
 a1_var = np.var(a1)
 
