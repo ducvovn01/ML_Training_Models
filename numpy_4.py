7f6f809872adcf7af606e86fd3a31c3bdb926d66
kieran <abc@gmail.com>
2026-01-09 13:37:52 +0700

    Adding histogram

--- numpy_4.py
+++ numpy_4.py
@@ -1,6 +1,10 @@
 #in this code, we learn about data distribution and histograms using numpy and matplotlib
-import numpy as np
+import numpy as 
+import matplotlib.pyplot as plt
 
 x = np.random.uniform(0.0, 5.0, 250)
+plt.hist(x, 10)
+
 
 print (x)
+plt.show()
