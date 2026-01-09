fefb539b1011407d5eb56cfe263f592656b25f73
kieran <abc@gmail.com>
2026-01-09 13:38:23 +0700

    fixing small error

--- numpy_4.py
+++ numpy_4.py
@@ -1,5 +1,5 @@
 #in this code, we learn about data distribution and histograms using numpy and matplotlib
-import numpy as 
+import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.random.uniform(0.0, 5.0, 250)
